57f735912be7c5f7363cdecce9f2a002c26862b4
zhangzhen9434 <zhangzhen9434@outlook.com>
2020-06-27 14:29:24 +0800

    updated

--- js/sy.js
+++
@@ -1,7 +0,0 @@
-function search() {
-    if (document.getElementById("search_input").value != "") {
-        window.location.href = "https://www.baidu.com/s?word=" + document.getElementById("search_input").value;
-        document.getElementById("search_input").value = "";
-    }
-    return false;
-}
